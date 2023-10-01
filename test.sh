ddad853ae537f166d5ac50c39c2a192dad9c4005
Yauheni Mazurau <mazurov1804@gmail.com>
2023-10-01 18:21:46 +0000

    SCRIPT RELEASE V. 1.0

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
 #!bin/bash
 
 cd /home/ym/03.Git/repo
-# Получить список удаленных репозиториев из .git/config
-#remote_names=($(grep -E '\[remote\.[^\]]+\]' .git/config | awk -F '.' '{print $2}'))
-
+# Получить массив удаленных репозиториев из .git/config
 remote_names=(`git remote`)
-# Перебрать каждый удаленный репозиторий и выполнить git push
+# Перебрать каждый удаленный репозиторий из массива и выполнить git push
 for remote_name in "${remote_names[@]}"
 do
   echo "Выполняется push в удаленный репозиторий: ${remote_name}"
