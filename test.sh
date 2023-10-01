ab12df2d9919e47f482590aebb202226ce89cbb5
Yauheni Mazurau <mazurov1804@gmail.com>
2023-10-01 16:52:50 +0000

    Test bash script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+cd /home/ym/03.Git/repo
+# Получить список удаленных репозиториев из .git/config
+remote_names=($(grep -E '\[remote\.[^\]]+\]' .git/config | awk -F '.' '{print $2}'))
+
+# Перебрать каждый удаленный репозиторий и выполнить git push
+for remote_name in "${remote_names[@]}"
+do
+  echo "Выполняется push в удаленный репозиторий: ${remote_name}"
+  git push "${remote_name}"
+done
